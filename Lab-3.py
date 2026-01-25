318bf8e189ee08de8dd4397a717557ff64519268
mahesh95-bit <maheshsudha680@gmail.com>
2026-01-25 14:17:33 +0530

    Update Lab-3.py
    Added matrix inversion classifier and comparison function for Minkowski distance.

--- Lab-3.py
+++ Lab-3.py
@@ -56,6 +56,13 @@ def minkowski_distance_custom(a, b, p):
     return s ** (1 / p)
 
 
+#A5
+def minkowski_compare(a, b, p):
+    custom = minkowski_distance_custom(a, b, p)
+    scipy_val = minkowski(a, b, p)
+    return custom, scipy_val
+
+
 #A6
 def euclidean_distance(a, b):
     return np.sqrt(np.sum((a - b) ** 2))
@@ -82,6 +89,15 @@ def confusion_metrics(y_true, y_pred):
     f1 = 2 * precision * recall / (precision + recall)
     return cm, accuracy, precision, recall, f1
 
+
+#A14
+def matrix_inversion_classifier(X_train, y_train, X_test):
+    Xb = np.c_[np.ones(X_train.shape[0]), X_train]
+    W = np.linalg.pinv(Xb.T @ Xb) @ Xb.T @ y_train
+    Xtb = np.c_[np.ones(X_test.shape[0]), X_test]
+    preds = Xtb @ W
+    return np.where(preds >= 0.5, 1, 0)
+
 np.random.seed(0)
 
 class0 = np.random.normal(2, 1, (50, 2))
@@ -105,6 +121,7 @@ centroid_distance = np.linalg.norm(centroid0 - centroid1)
 
 feature = X[:, 0]
 hist_data = np.histogram(feature, bins=10)
+feat_mean, feat_var = feature_stats(feature)
 
 plt.hist(feature, bins=10)
 plt.show()
@@ -115,21 +132,26 @@ distances = [minkowski_distance_custom(a, b, p) for p in p_values]
 plt.plot(p_values, distances)
 plt.show()
 
-custom_mink = minkowski_distance_custom(a, b, 3)
-scipy_mink = minkowski(a, b, 3)
+custom_mink, scipy_mink = minkowski_compare(a, b, 3)
 
+#A6
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
+#A7
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
+
+#A8
 accuracy_knn = knn.score(X_test, y_test)
 
+#A9
 pred_knn = knn.predict(X_test)
+single_vector_prediction = knn.predict([X_test[0]])
 
+#A10
 pred_custom = np.array([knn_predict_custom(X_train, y_train, x, 3) for x in X_test])
 
-cm, acc, prec, rec, f1 = confusion_metrics(y_test, pred_knn)
-
+#A11
 k_vals = range(1, 12)
 accs = []
 for k in k_vals:
@@ -139,3 +161,11 @@ for k in k_vals:
 plt.plot(k_vals, accs)
 plt.show()
 
+#A12
+train_preds = knn.predict(X_train)
+cm_train, acc_train, prec_train, rec_train, f1_train = confusion_metrics(y_train, train_preds)
+cm_test, acc_test, prec_test, rec_test, f1_test = confusion_metrics(y_test, pred_knn)
+
+#A14
+inv_preds = matrix_inversion_classifier(X_train, y_train, X_test)
+cm_inv, acc_inv, prec_inv, rec_inv, f1_inv = confusion_metrics(y_test, inv_preds)
